7a628e20b11f96d8222d6651fa79103e5612cf69
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2024-04-11 21:12:39 +0300

    add env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,16 @@
 import { getAuth } from 'firebase/auth';
 
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJgCglI9UqQg6Y9tWlR-LQlB-iXCBDxOw',
-  authDomain: 'learn-lingo-54f49.firebaseapp.com',
-  projectId: 'learn-lingo-54f49',
-  storageBucket: 'learn-lingo-54f49.appspot.com',
-  messagingSenderId: '353984341419',
-  appId: '1:353984341419:web:4350b2c3b17c4048689eee',
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
