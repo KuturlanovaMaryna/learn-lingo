1baccba77996f32092a33eb44b3783a7040ad836
Maryna <marimariovod@gmail.com>
2024-04-16 21:45:19 +0300

    add modal container

---
+++ src/components/Modal/Modal.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Modal = styled.div`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: grey;
+  overflow: auto;
+  z-index: 5;
+
+  transform: scale(0);
+  &.active {
+    transform: scale(1);
+  }
+`;
