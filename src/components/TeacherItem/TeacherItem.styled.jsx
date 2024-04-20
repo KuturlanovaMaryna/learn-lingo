4476cb2f17cd6091a6575958e04f67b1d35029f6
Maryna <marimariovod@gmail.com>
2024-04-20 22:11:23 +0300

    add block in teacher card and add favicon

--- src/components/TeacherItem/TeacherItem.styled.jsx
+++ src/components/TeacherItem/TeacherItem.styled.jsx
@@ -7,6 +7,8 @@ export const Card = styled.div`
   border-radius: 24px;
   min-height: 328px;
   padding: 24px;
+  display: flex;
+  gap: 48px;
 `;
 export const ImageWrapper = styled.div`
   padding: 24px;
@@ -36,4 +38,32 @@ export const GreenDot = styled(GoDotFill)`
   stroke-width: 2px;
 
   border-radius: 100px;
+`;
+export const InfoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 8px;
+`;
+export const NameBlock = styled.div``;
+export const Title = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.5;
+  color: #8a8a89;
+`;
+export const FullName = styled.p`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 100%;
+  color: #121417;
+`;
+export const MainInfo = styled.div`
+  display: flex;
+  margin-left: 100px;
+`;
+export const MainBlockInfo = styled.span`
+  display: inline-flex;
+
+  padding-left: 16px;
+  padding-right: 16px;
 `;
--- src/components/TeacherItem/Teachertem.jsx
+++ src/components/TeacherItem/Teachertem.jsx
@@ -1,13 +1,23 @@
 import {
   Card,
+  FullName,
   GreenDot,
   ImageWrapper,
+  InfoWrapper,
+  MainBlockInfo,
+  MainInfo,
+  NameBlock,
   PhotoFrame,
   TeacherPhoto,
+  Title,
 } from './TeacherItem.styled';
+import { IoBookOutline } from 'react-icons/io5';
+import { RxDividerVertical } from 'react-icons/rx';
+import { IoMdStar } from 'react-icons/io';
 
 export const TeacherItem = ({ card }) => {
-  const { avatar_url, name, surname } = card;
+  const { avatar_url, name, surname, lessons_done, rating, price_per_hour } =
+    card;
 
   return (
     <Card>
@@ -17,6 +27,29 @@ export const TeacherItem = ({ card }) => {
           <GreenDot size="20" />
         </PhotoFrame>
       </ImageWrapper>
+      <InfoWrapper>
+        <NameBlock>
+          <Title>Languages</Title>
+          <FullName>
+            {name} {surname}
+          </FullName>
+        </NameBlock>
+        <MainInfo>
+          <MainBlockInfo>
+            <IoBookOutline size={16} />
+            <span style={{ paddingLeft: 8 }}>Lenguages</span>
+          </MainBlockInfo>
+          <RxDividerVertical color="#121417" opacity={0.2} />
+          <MainBlockInfo>Lessons done:{lessons_done}</MainBlockInfo>{' '}
+          <RxDividerVertical color="#121417" opacity={0.2} />
+          <MainBlockInfo>
+            <IoMdStar color="#FFC531" size={16} />
+            <span style={{ paddingLeft: 8 }}>Rating:{rating}</span>
+          </MainBlockInfo>
+          <RxDividerVertical color="#121417" opacity={0.2} />
+          <MainBlockInfo>Price / 1 hour: {price_per_hour}$</MainBlockInfo>
+        </MainInfo>
+      </InfoWrapper>
     </Card>
   );
 };
