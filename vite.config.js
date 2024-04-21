6ded35d63ab93ea7270c70f8ec997d5d9adbf85a
Maryna <marimariovod@gmail.com>
2024-04-21 19:50:45 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default defineConfig({
   plugins: [react(), svgr()],
+  define: {
+    'process.env': process.env,
+  },
   resolve: {
     alias: {
       src: '/src',
